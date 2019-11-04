6254995ce346015d937f0c634cf00392e77f575e
c413bxd <caleb.895.9@gmail.com>
2019-11-03 22:48:19 -0500

    para-completar-menu

--- src/pages/menus/Mjuegos/MenuJuego.vue
+++ src/pages/menus/Mjuegos/MenuJuego.vue
@@ -1,72 +1,78 @@
 <template>
-  <q-page-container>
-    <q-page>
-      <template>
-  <div class="q-pa-md row justify-center">
-    <template>
-  <div class="  q-gutter-md">
-    <q-card class="my-card">
-      <q-card-section class="bg-primary text-white">
-        <div class="text-h6">Our Changing Planet</div>
-        <div class="text-subtitle2">by John Doe</div>
-      </q-card-section>
-
-      <q-separator />
-
-      <q-card-actions >
-        <q-btn flat>Action 1</q-btn>
-        <q-btn flat>Action 2</q-btn>
-      </q-card-actions>
-    </q-card>
-
-    <q-card class="my-card">
-      <q-card-section class="bg-purple text-white">
-        <div class="text-h6">Our Changing Planet</div>
-        <div class="text-subtitle2">by John Doe</div>
-      </q-card-section>
-
-      <q-card-actions align="around">
-        <q-btn flat>Action 1</q-btn>
-        <q-btn flat>Action 2</q-btn>
-      </q-card-actions>
-    </q-card>
-
-    <q-card class="my-card">
-      <q-card-section class="bg-teal text-white">
-        <div class="text-h6">Our Changing Planet</div>
-        <div class="text-subtitle2">by John Doe</div>
-      </q-card-section>
-
-      <q-card-actions vertical align="right">
-        <q-btn flat>Action 1</q-btn>
-        <q-btn flat>Action 2</q-btn>
-      </q-card-actions>
-    </q-card>
-
-    <q-card class="my-card">
-      <q-card-section class="bg-grey-8 text-white">
-        <div class="text-h6">Our Changing Planet</div>
-        <div class="text-subtitle2">by John Doe</div>
-      </q-card-section>
-
-      <q-card-actions vertical align="center">
-        <q-btn flat>Action 1</q-btn>
-        <q-btn flat>Action 2</q-btn>
-      </q-card-actions>
-    </q-card>
-  </div>
-</template>
-
-  </div>
-</template>
+    <q-page class="container q-ma-lg">
+            <!-- <pre class="q-ma-none container">{{ scrollInfo.position }}</pre>
+            <q-scroll-observer @scroll="onScroll" /> -->
+      <h2 class="text-center">Juegos</h2>      
+      <div class="row q-gutter-lg justify-center">
+        <div class="col-md-3 col-xs-12" v-for="item in 10" data-aos="fade-up" data-aos-duration="1500">
+            <q-card class="my-card">
+                <img spinner-color="orange" src="https://cdn.quasar.dev/img/mountains.jpg">
+          
+                <q-card-section>
+                  <div class="text-h6 text-center">Our Changing Planet</div>
+                  <div class="text-subtitle2 text-center">by John Doe</div>
+                </q-card-section>
+      
+                <q-card-section>
+                  <!-- {{ lorem }} -->
+                </q-card-section>
+              </q-card>
+        </div>
+      </div>
+        <!-- <div class="col">
+            <q-card class="my-card">
+                <img src="https://cdn.quasar.dev/img/mountains.jpg">
+          
+                <q-card-section>
+                  <div class="text-h6">Our Changing Planet</div>
+                  <div class="text-subtitle2">by John Doe</div>
+                </q-card-section>
+          
+                <q-card-section>
+                  {{ lorem }}
+                </q-card-section>
+              </q-card>
+        </div>
+      </div> -->
+      
+      
     </q-page>
-  </q-page-container>
 </template>
 <script>
+import AOS from "aos"
+import 'aos/dist/aos.css';
 export default {
-    
+
+    data(){
+      return{
+        scrollInfo: {}
+      }
+    },
+    created(){
+      AOS.init();
+    },
+    methods: {
+    onScroll (info) {
+      this.scrollInfo = info
+    }
+  }
 }
 </script>
 <style>
+  
+/* .container{
+  animation: transitionIn 2s;
+}
+@keyframes transitionIn{
+    from {
+      opacity: 0;
+      transform: translateY(-10px);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+
+}   */
 
 </style>
