6c323fe5f89c6a130e51445e19280738cc27ca8c
c413bxd <caleb.895.9@gmail.com>
2021-09-10 02:02:09 -0500

    modulo numeros

--- src/pages/views/Numeros.vue
+++ src/pages/views/Numeros.vue
@@ -1,7 +1,7 @@
 <template>
     
     <div class="q-pa-md">
-        <h4>Los numeros del 0 - 100</h4>
+        <!-- <h4>Los numeros del 0 - 100</h4>
         <q-table
         title="Los Numeros en Aymara"
         :data="data"
@@ -9,124 +9,192 @@
         row-key="id"
         
         color="amber"
-      />
+      /> -->
+      <div class="contenedor">
+        <div class="q-px-md titulo text-h1 text-bold text-grey-9 text-center content-center">
+          Los numeros
+          <ul class="text-subtitle2 text-left">
+            <li>
+              Jakhu				: número
+            </li>
+            <li>
+              Naka				: sufijo pluralizador
+            </li>
+            <li>
+              Jakhuña			: contar (número)
+            </li>
+            <li>
+              Jakhuñani			: contemos
+            </li>
+          </ul> 
+          <p class="text-subtitle2 q-ma-none">
+            Para escribir cifras mayores es necesario descomponer las cifras, y así poder escribir grandes cantidades.
+          </p>
+          <p class="text-subtitle2 text-left q-ma-none">
+            <ul>
+              <li>100	 Pataka,
+              101	 Pataka mayani,
+              102	 Pataka payani</li>
+              <li>124	 Pataka pä tunka pusini = <span class="text-bold">100: pataka
+                                            ->20: pätunka
+                                              ->4: pusini</span></li>
+              <li>200	 Pä pataka,
+              300	 Kimsa pataka,
+              400	 Pusi pataka,
+              500	 Phisqa pataka</li>
+              <li>1000	 Waranqa – waranka [waranga]</li>
+              <li>1001	 Waranqa mayani [waranga mayani]</li>
+              <li>1111	 Waranqa pataka tunka mayani</li>
+              <li>1432	 Waranqa pusi pataka kimsa tunka payani	= <span class="text-bold">1000: waranqa
+                                                                  ->400: pusi pataka
+                                                                    ->30: kimsa tunka
+                                                                      ->2: payani</span></li>
+            </ul>
+          </p>
+        </div>
+        <div class="container" v-for="(item,key,index) in objetos">
+          <div class="card">
+            <div class="circle bg-indigo text-white text-h2 text-bold text-center">
+              {{key}}
+            </div>
+            <div class="content">
+              <div v-for="(numeros,index) in 10">
+                <span class="text-bold">{{item[index].id}}</span> {{item[index].name}}
+              </div>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+
+      <!-- componente -->
+      <!-- <div class="container">
+        <div class="card">
+          <div class="circle text-white text-h2 text-bold text-center">
+            hola
+          </div>
+          <div class="content">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab illum praesentium, commodi, in esse deserunt tempora tenetur, nulla magni tempore nihil ad aut nam mollitia repellendus quam voluptas quod quibusdam?</p>
+          </div>
+        </div>
+      </div> -->
+      
     </div>
   </template>
 <script>
+  import objetos from "../../vocabulario/numeros.json"
 export default {
     data(){
         return{
-            columns: [
-            { name: 'numeros', align: 'center', label: 'Numeros', field: 'id', sortable: true },
-            { name: 'aymara', align: 'center',label: 'Numeros en Aymara', field: 'name'},
-            ],
-            data: [
-            {name:"ch’usa, Muruq’u, Muyuga", id:0, color:"bg-purple",check:null},
-            {name:"Mayaaaaa", id:1, color:"bg-indigo",check:null},
-            {name:"Paya", id:2,color:"bg-cyan",check:null},
-            {name:"Kimsa", id:3, color:"bg-lime",check:null},
-            {name:"Pusi", id:4, color:"bg-teal",check:null},
-            {name:"Phisqa", id:5, color:"bg-brown",check:null},
-            {name:"Suxta", id:6, color:"bg-deep-orange",check:null},
-            {name:"Paqallqu", id:7, color:"bg-blue-grey",check:null},
-            {name:"Kimsaqallqu", id:8, color:"bg-deep-purple",check:null},
-            {name:"Llätunka", id:9, color:"bg-light-blue",check:null},
-            
-
-            {name:"tunka", id:10, color:"bg-light-blue",check:null},
-            {name:"tunka-mayani", id:11, color:"bg-light-blue",check:null},
-            {name:"tunka-payani", id:12, color:"bg-light-blue",check:null},
-            {name:"tunka-kimsani", id:13, color:"bg-light-blue",check:null},
-            {name:"tunka-pusini", id:14, color:"bg-light-blue",check:null},
-            {name:"tunka-phiscani", id:15, color:"bg-light-blue",check:null},
-            {name:"tunka-suxtani", id:16, color:"bg-light-blue",check:null},
-            {name:"tunka-paqalquni", id:17, color:"bg-light-blue",check:null},
-            {name:"tunka-kimsaqalquni", id:18, color:"bg-light-blue",check:null},
-            {name:"tunka-llatunkani", id:19, color:"bg-light-blue",check:null},
-            {name:"pätunka", id:20, color:"bg-light-blue",check:null},
-
-            {name:"pätunka-mayani", id:21, color:"bg-light-blue",check:null},
-            {name:"pätunka-payani", id:22, color:"bg-light-blue",check:null},
-            {name:"pätunka-kimsani", id:23, color:"bg-light-blue",check:null},
-            {name:"pätunka-pusini", id:24, color:"bg-light-blue",check:null},
-            {name:"pätunka-phiscani", id:25, color:"bg-light-blue",check:null},
-            {name:"pätunka-suxtani", id:26, color:"bg-light-blue",check:null},
-            {name:"pätunka-paqalquni", id:27, color:"bg-light-blue",check:null},
-            {name:"pätunka-kimsaqalquni", id:28, color:"bg-light-blue",check:null},
-            {name:"pätunka-llatunkani", id:29, color:"bg-light-blue",check:null},
-            {name:"kimsa-tunka", id:30, color:"bg-light-blue",check:null},
-            {name:"kimsa-tunka-mayani", id:31, color:"bg-light-blue",check:null},
-            {name:"kimsa-tunka-payani", id:32, color:"bg-light-blue",check:null},
-            {name:"kimsa-tunka-kimsani", id:33, color:"bg-light-blue",check:null},
-            {name:"kimsa-tunka-pusini", id:34, color:"bg-light-blue",check:null},
-            {name:"kimsa-tunka-phiscani", id:35, color:"bg-light-blue",check:null},
-            {name:"kimsa-tunka-suxtani", id:36, color:"bg-light-blue",check:null},
-            {name:"kimsa-tunka-paqalquni", id:37, color:"bg-light-blue",check:null},
-            {name:"kimsa-tunka-kimsaqalquni", id:38, color:"bg-light-blue",check:null},
-            {name:"kimsa-tunka-llatunkani", id:39, color:"bg-light-blue",check:null},
-            {name:"pusi-tunka", id:40, color:"bg-light-blue",check:null},
-            {name:"pusi-tunka-mayani", id:41, color:"bg-light-blue",check:null},
-            {name:"pusi-tunka-payani", id:42, color:"bg-light-blue",check:null},
-            {name:"pusi-tunka-kimsani", id:43, color:"bg-light-blue",check:null},
-            {name:"pusi-tunka-pusini", id:44, color:"bg-light-blue",check:null},
-            {name:"pusi-tunka-phiscani", id:45, color:"bg-light-blue",check:null},
-            {name:"pusi-tunka-suxtani", id:46, color:"bg-light-blue",check:null},
-            {name:"pusi-tunka-paqalquni", id:47, color:"bg-light-blue",check:null},
-            {name:"pusi-tunka-kimsaqalquni", id:48, color:"bg-light-blue",check:null},
-            {name:"pusi-tunka-llatunkani", id:49, color:"bg-light-blue",check:null},
-            {name:"phisca-tunka", id:50, color:"bg-light-blue",check:null},
-            {name:"phisca-tunka-mayani", id:51, color:"bg-light-blue",check:null},
-            {name:"phisca-tunka-payani", id:52, color:"bg-light-blue",check:null},
-            {name:"phisca-tunka-kimsani", id:53, color:"bg-light-blue",check:null},
-            {name:"phisca-tunka-pusini", id:54, color:"bg-light-blue",check:null},
-            {name:"phisca-tunka-phiscani", id:55, color:"bg-light-blue",check:null},
-            {name:"phisca-tunka-suxtani", id:56, color:"bg-light-blue",check:null},
-            {name:"phisca-tunka-paqalquni", id:57, color:"bg-light-blue",check:null},
-            {name:"phisca-tunka-kimsaqalquni", id:58, color:"bg-light-blue",check:null},
-            {name:"phisca-tunka-llatunkani", id:59, color:"bg-light-blue",check:null},
-            {name:"suxta-tunka", id:60, color:"bg-light-blue",check:null},
-            {name:"suxta-tunka-mayani", id:61, color:"bg-light-blue",check:null},
-            {name:"suxta-tunka-payani", id:62, color:"bg-light-blue",check:null},
-            {name:"suxta-tunka-kimsani", id:63, color:"bg-light-blue",check:null},
-            {name:"suxta-tunka-pusini", id:64, color:"bg-light-blue",check:null},
-            {name:"suxta-tunka-phiscani", id:65, color:"bg-light-blue",check:null},
-            {name:"suxta-tunka-suxtani", id:66, color:"bg-light-blue",check:null},
-            {name:"suxta-tunka-paqalquni", id:67, color:"bg-light-blue",check:null},
-            {name:"suxta-tunka-kimsaqalquni", id:68, color:"bg-light-blue",check:null},
-            {name:"suxta-tunka-llatunkani", id:69, color:"bg-light-blue",check:null},
-            {name:"paqalqu-tunka", id:70, color:"bg-light-blue",check:null},
-            {name:"paqalqu-tunka-mayani", id:71, color:"bg-light-blue",check:null},
-            {name:"paqalqu-tunka-payani", id:72, color:"bg-light-blue",check:null},
-            {name:"paqalqu-tunka-kimsani", id:73, color:"bg-light-blue",check:null},
-            {name:"paqalqu-tunka-pusini", id:74, color:"bg-light-blue",check:null},
-            {name:"paqalqu-tunka-phiscani", id:75, color:"bg-light-blue",check:null},
-            {name:"paqalqu-tunka-suxtani", id:76, color:"bg-light-blue",check:null},
-            {name:"paqalqu-tunka-paqalquni", id:77, color:"bg-light-blue",check:null},
-            {name:"paqalqu-tunka-kimsaqalquni", id:78, color:"bg-light-blue",check:null},
-            {name:"paqalqu-tunka-llatunkani", id:79, color:"bg-light-blue",check:null},
-            {name:"kimsaqalqu-tunka", id:80, color:"bg-light-blue",check:null},
-            {name:"kimsaqalqu-tunka-mayani", id:81, color:"bg-light-blue",check:null},
-            {name:"kimsaqalqu-tunka-payani", id:82, color:"bg-light-blue",check:null},
-            {name:"kimsaqalqu-tunka-kimsani", id:83, color:"bg-light-blue",check:null},
-            {name:"kimsaqalqu-tunka-pusini", id:84, color:"bg-light-blue",check:null},
-            {name:"kimsaqalqu-tunka-phiscani", id:85, color:"bg-light-blue",check:null},
-            {name:"kimsaqalqu-tunka-suxtani", id:86, color:"bg-light-blue",check:null},
-            {name:"kimsaqalqu-tunka-paqalquni", id:87, color:"bg-light-blue",check:null},
-            {name:"kimsaqalqu-tunka-kimsaqalquni", id:88, color:"bg-light-blue",check:null},
-            {name:"kimsaqalqu-tunka-llatunkani", id:89, color:"bg-light-blue",check:null},
-            {name:"llatunka-tunka", id:90, color:"bg-light-blue",check:null},
-            {name:"llatunka-tunka-mayani", id:91, color:"bg-light-blue",check:null},
-            {name:"llatunka-tunka-payani", id:92, color:"bg-light-blue",check:null},
-            {name:"llatunka-tunka-kimsani", id:93, color:"bg-light-blue",check:null},
-            {name:"llatunka-tunka-pusini", id:94, color:"bg-light-blue",check:null},
-            {name:"llatunka-tunka-phiscani", id:95, color:"bg-light-blue",check:null},
-            {name:"llatunka-tunka-suxtani", id:96, color:"bg-light-blue",check:null},
-            {name:"llatunka-tunka-paqalquni", id:97, color:"bg-light-blue",check:null},
-            {name:"llatunka-tunka-kimsaqalquni", id:98, color:"bg-light-blue",check:null},
-            {name:"llatunka-tunka-llatunkani", id:99, color:"bg-light-blue",check:null},
-            {name:"pataka", id:100, color:"bg-light-blue",check:null}
-            ]
+            objetos
         }
+    },
+    methods: {
+          hola(){
+            console.log(this.objetos)
+          }
     }
 }
 </script>
+<style>
+  .container{
+    position: relative;
+    overflow: hidden;
+    box-shadow: 0 15px 25px rgba(0, 0, 0, 0.5);
+    border-radius: 20px;
+  }
+  .container .card{
+    width: 100%;
+    height: 460px;
+  }
+  .container .circle{
+    position: relative;
+    width: 100%;
+    height: 100%;
+    /* background:blue; */
+    clip-path: circle(50% at center 0);
+    padding: 20% 0;
+  }
+  .container .card .content{
+    position:absolute;
+    top:45%;
+    padding: 0 5px 0 10px;
+  }
+  .contenedor{
+    display: grid;
+    /* border: solid black 10px; */
+    margin: auto;
+    /* font-size: 50px; */
+    /* line-height: 50px; */
+    /* text-align: center; */
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(3, auto);
+    grid-gap: 20px;
+    /* grid-auto-flow: dense; */
+    grid-template-areas: ". titulo titulo .";
+  }
+  .contenedor>div{
+    display: block;
+    /* background: chartreuse; */
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.5);
+    /* grid-area: card; */
+  }
+  .contenedor .titulo{
+    /* grid-column: 2/4; */
+    /* grid-row: 1/2; */
+    grid-area: titulo;
+    font-size: 5em;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
+
+
+
+  @media screen and (max-width:1024px){
+    .contenedor .titulo{
+      font-size: 4.5em;
+    }
+    .contenedor {
+      grid-template-columns: repeat(3, 1fr);
+      /* grid-template-rows: repeat(5, auto); */
+      grid-template-areas: " titulo titulo titulo ";
+      
+    }
+    .contenedor>div:last-child{
+      grid-column: 2/3;
+    }
+  }
+  @media screen and (max-width:768px){
+    .contenedor .titulo{
+      font-size: 4em;
+    }
+    .contenedor{
+      grid-template-columns: repeat(2, 1fr);
+      /* grid-template-rows: repeat(6, auto); */
+      grid-template-areas: " titulo titulo ";
+    }
+  }
+  @media screen and (max-width:624px){
+    .container .card .content{
+    font-size: 0.8em;
+  }
+  }
+  @media screen and (max-width:480px){
+    .container .card .content{
+    font-size: 1em;
+  }
+    .contenedor .titulo{
+      font-size: 3.7em;
+    }
+    .contenedor{
+      grid-template-columns: 1fr;
+      /* grid-template-rows: repeat(11, auto); */
+      grid-template-areas: "titulo";
+    }
+    .contenedor>div:last-child{
+      grid-column: 1/2;
+    }
+  }
+</style>
--- src/pages/views/vocabulario/cuerpo_humano.vue
+++ src/pages/views/vocabulario/cuerpo_humano.vue
@@ -2,7 +2,7 @@
     <div>
       <div>
         <div class="row justify-between">
-          <h2 class="q-ma-none q-mb-md">Palabras comunes de animales</h2>
+          <h2 class="q-ma-none q-mb-md">Palabras comunes del cuerpo humano</h2>
           <!-- <button @click="buscar()">buscar</button> -->
           <q-input
             outlined
@@ -15,7 +15,66 @@
             </template>
           </q-input>
         </div>
+        <div class="row">
+          
+          <div class="col-4">
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12 bg-orange-1 text-orange text-center q-pa-sm border-naranja">
+                <q-btn round color="orange" icon="volume_up" />
+              </div>
+              <div class="col-md-6 col-sm-6 col-xs-12 bg-indigo-1 text-center border q-pa-md">lore</div>
+              <div class="col-md-6 col-sm-6 col-xs-12 bg-blue-1 border text-center q-pa-md">estas</div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12 bg-orange-1 text-orange text-center q-pa-sm border-naranja">
+                <q-btn round color="orange" icon="volume_up" />
+              </div>
+              <div class="col-md-6 col-sm-6 col-xs-12 bg-indigo-1 text-center border q-pa-md">lore</div>
+              <div class="col-md-6 col-sm-6 col-xs-12 bg-blue-1 border text-center q-pa-md">estas</div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12 bg-orange-1 text-orange text-center q-pa-sm border-naranja">
+                <q-btn round color="orange" icon="volume_up" />
+              </div>
+              <div class="col-md-6 col-sm-6 col-xs-12 bg-indigo-1 text-center border q-pa-md">lore</div>
+              <div class="col-md-6 col-sm-6 col-xs-12 bg-blue-1 border text-center q-pa-md">estas</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-12 col-sm-12 col-xs-12 bg-orange-1 text-orange text-center q-pa-sm border-naranja">
+            <q-btn round color="orange" icon="volume_up" />
+          </div>
+          <div class="col-md-6 col-sm-6 col-xs-12 bg-indigo-1 text-center border q-pa-md">lore</div>
+          <div class="col-md-6 col-sm-6 col-xs-12 bg-blue-1 border text-center q-pa-md">estas</div>
+        </div>
       </div>
+
+      <div class="row position_rel">
+        <div class="col-12 bg-indigo-1 text-center border q-pa-md">
+          <div class="row justify-end">
+            <q-btn class="position_btn" round color="orange" icon="volume_up" />
+          </div>
+          lore
+        </div>
+        <div class="col-12 bg-blue-1 border text-center q-pa-md">estas</div>
+      </div>
+
+      <div class="row position_rel">
+        <div class="col-12 bg-indigo text-center text-white border q-pa-md">
+          <div class="row justify-end">
+            <q-btn class="position_btn" round color="orange" icon="volume_up" />
+          </div>
+          lore
+        </div>
+        <div class="col-12 bg-secondary border text-center text-white q-pa-md">estas</div>
+      </div>
+
       <cards :ObjetoFiltrado="TextoFiltrado"></cards>
     </div>
   </template>
@@ -48,5 +107,25 @@
     }
   };
   </script>
-  <style></style>
+  <style>
+    .border{
+      border: 1px blue ridge;
+      /* position: relative; */
+    }
+    .border-naranja{
+      border-top: 1px orange ridge;
+      border-left: 1px orange ridge;
+      border-right: 1px orange ridge;
+      border-radius: 5px 5px 0px 0px;
+    }
+    .position_rel{
+      position: relative;
+    }
+    .position_btn{
+      position: absolute;
+      margin: 0;
+      top: 30px;
+      /* left: 98%; */
+    }
+  </style>
   
