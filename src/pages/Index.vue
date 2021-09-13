d99f463064a999f809fcb40a4ae451bbd2e3e060
c413bxd <caleb.895.9@gmail.com>
2021-09-13 17:31:47 -0500

    modulo animales v2

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -61,17 +61,18 @@
             
             <q-tab-panel name="palabras">
               <div class="text-h6">Palabras acerca de animales</div>
-              <cards :ObjetoFiltrado="palabrasAnimal"></cards>
+              <!-- <cards :ObjetoFiltrado="palabrasAnimal"></cards> -->
+              <lista :ObjetoFiltrado="palabrasAnimal"></lista>
             </q-tab-panel>
           </q-tab-panels>
         </q-card>
       </div>
 <!-- ============================================================== -->
     </div>
-    <div>
+    <!-- <div>
       <div class="row justify-between">
         <h2 class="q-ma-none q-mb-md">Saminaka / Colores</h2>
-        <!-- <button @click="buscar()">buscar</button> -->
+        <button @click="buscar()">buscar</button>
         <q-input
           outlined
           v-model="TextoBuscado"
@@ -83,10 +84,7 @@
           </template>
         </q-input>
       </div>
-    </div>
-    <!-- <cards :ObjetoFiltrado="TextoFiltrado"></cards> -->
-    <!-- <cards :ObjetoFiltrado="colores"></cards> -->
-    <!-- <cards :ObjetoFiltrado="silvestres"></cards> -->
+    </div> -->
   </div>
   
 </template>
@@ -94,6 +92,7 @@
 <script>
 // import vueFlashcard from "vue-flashcard";
 import cards from "components/cards.vue";
+import lista from "components/lista_sound.vue";
 import domesticos from "../vocabulario/animal_domestico.json";
 // import domesticos from "../vocabulario/arbustos_arboles.json";
 import colores from "../vocabulario/colores.json";
@@ -104,7 +103,7 @@ import palabrasAnimal from "../vocabulario/animales.json";
 
 export default {
   name: "PageIndex",
-  components: { cards },
+  components: { cards, lista },
   data() {
     return {
       tab: 'domesticos',
@@ -131,4 +130,80 @@ export default {
   }
 };
 </script>
-<style></style>
+<style>
+  .contenedor{
+    display: grid;
+    /* border: solid black 10px; */
+    margin: auto;
+    /* font-size: 50px; */
+    /* line-height: 50px; */
+    /* text-align: center; */
+    grid-template-columns: repeat(5, 1fr);
+    /* grid-template-rows: repeat(3, auto); */
+    grid-gap: 20px;
+    /* grid-auto-flow: dense; */
+    grid-template-areas: "card card card card card";
+  }
+  .contenedor>div{
+    /* display: block; */
+    /* background: chartreuse; */
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.5);
+    /* grid-area: card; */
+  }
+  .contenedor .titulo{
+    /* grid-column: 2/4; */
+    /* grid-row: 1/2; */
+    grid-area: titulo;
+    font-size: 5em;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
+
+  @media screen and (max-width:1024px){
+    .contenedor .titulo{
+      font-size: 4.5em;
+    }
+    .contenedor {
+      grid-template-columns: repeat(3, 1fr);
+      /* grid-template-rows: repeat(5, auto); */
+      grid-template-areas: " card card card ";
+      
+    }
+    .contenedor>div:last-child{
+      grid-column: 2/3;
+    }
+  }
+  @media screen and (max-width:768px){
+    .contenedor .titulo{
+      font-size: 4em;
+    }
+    .contenedor{
+      grid-template-columns: repeat(2, 1fr);
+      /* grid-template-rows: repeat(6, auto); */
+      grid-template-areas: " card card ";
+    }
+  }
+  @media screen and (max-width:624px){
+    .container .card .content{
+    font-size: 0.8em;
+  }
+  }
+  @media screen and (max-width:480px){
+    .container .card .content{
+    font-size: 1em;
+  }
+    .contenedor .titulo{
+      font-size: 3.7em;
+    }
+    .contenedor{
+      grid-template-columns: 1fr;
+      /* grid-template-rows: repeat(11, auto); */
+      grid-template-areas: "card";
+    }
+    .contenedor>div:last-child{
+      grid-column: 1/2;
+    }
+  }
+</style>
