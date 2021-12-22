d7c405486c641a400b94f94fcc7b3d8213b6938b
waready <elbarto119@hotmail.com>
2021-12-21 22:32:07 -0500

    deploy

---
+++ dist/spa/js/36.12d40fdf.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[36],{c64a:function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div")},l=[],u={},a=u,r=t("2877"),s=Object(r["a"])(a,c,l,!1,null,null,null);e["default"]=s.exports}}]);
